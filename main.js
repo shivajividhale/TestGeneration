24debb4b1f645f05612f68465f5b0a70d68775b4
shivajividhale <shivajividhale@gmail.com>
2015-10-13 22:17:25 -0400

    28/30 cleaned

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ String.prototype.replaceAt=function(index, character) {
 }
 function fakeDemo()
 {
-	console.log( "Phone number: "+faker.phone.phoneNumber('NNN-NNN-NNNN') );
+	console.log( "Phone number: "+faker.phone.phoneNumber('###-###-####') );
 	console.log( "Phone number format: "+faker.phone.phoneNumberFormat() );
 	console.log( "Phone formats: "+faker.phone.phoneFormats() );
 }
@@ -100,36 +100,28 @@ function generateTestCases()
 		var fileWithContent = _.some(constraints, {kind: 'fileWithContent' });
 		var pathExists      = _.some(constraints, {kind: 'fileExists' });
 		var Phone_Input		= _.some(constraints, {kind: 'phoneNumber' });
-		if(Phone_Input)
-		console.log("file with contents "+fileWithContent);
-		console.log("path exists "+pathExists);
-		console.log("For "+funcName);
+
 		var blacklist = "";
 		for( var c = 0; c < constraints.length; c++ )
 		{
 			var constraint = constraints[c];
 
 			if(Phone_Input){
-				console.log("phone input!!!!!!!!!!!!!!!!!!");
+				//Create a number for later replacing with  area code to blacklist
 				blacklist = faker.phone.phoneNumber("###-###-###");
-				console.log(blacklist);
 			}
-			if(constraint.operator == "!"){
-				console.log("WOOAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH");
+			if(Phone_Input && constraint.operator == "!"){
 				params[constraint.ident] = constraint.value;
 				var args = Object.keys(params).map( function(k) {return params[k]; }).join(",");
 				content += "subject.{0}({1});\n".format(funcName, args );
-
 			}
+
+			//For dealing with area
 			if(Phone_Input && constraint.operator == "=="){
 				var args = Object.keys(params).map( function(k) {return params[k]; }).join(",");
 				content += "subject.{0}({1});\n".format(funcName, args );
 				blacklist = blacklist.replaceAt(0,constraint.value.substr(1,constraint.value.length-1));
 				blacklist = blacklist.replace("\"","-")
-				//var numbers = blacklist.split("-");
-				//numbers[0] = parseInt(constraint.value.substr(1,constraint.value.length-1));
-				//var phonenum = numbers.join("");
-				console.log("After replacing "+blacklist);
 				blacklist = "\""+blacklist+"\"";
 				var args = Object.keys(params).map( function(k) {return params[k]; }).join(",");
 				content += "subject.{0}({1});\n".format(funcName, blacklist );
@@ -137,12 +129,8 @@ function generateTestCases()
 			}
 
 			//console.log("Constraint: "+JSON.stringify(constraint));
-			console.log(constraint.expression);
 			if( params.hasOwnProperty( constraint.ident ) ){
-				console.log("indent: "+constraint.ident+" value: "+constraint.value);
 				params[constraint.ident]=constraint.value;//constraint.value;
-
-
 
 				if (constraint.value == 'undefined' && constraint.kind == 'integer'){
 					params[constraint.ident]=createConcreteIntegerValue(0,10);
@@ -195,7 +183,6 @@ function generateTestCases()
 			// Bonus...generate constraint variations test cases....
 		}
 		var args = Object.keys(params).map( function(k) {return params[k]; }).join(",");
-		console.log("Params: "+JSON.stringify(params)+"Arguments: "+args+"Key: "+JSON.stringify(params));
 		if( pathExists || fileWithContent )
 		{
 			content += generateMockFsTestCases(pathExists,fileWithContent,funcName, args,true);
@@ -207,8 +194,6 @@ function generateTestCases()
 		{
 			// Emit simple test case.
 			content += "subject.{0}({1});\n".format(funcName, args );
-			console.log("Args: "+args);
-			//console.log(content);
 		}
 
 	}
@@ -225,8 +210,7 @@ function generateMockFsTestCases (pathExists,fileWithContent,funcName,args, recu
 	var mergedFS = {};
 	if( pathExists )
 	{
-		for (var attrname in mockFileLibrary.pathExists) { mergedFS[attrname] = mockFileLibrary.pathExists[attrname];
-		console.log("merge attr"+ JSON.stringify(mergedFS));}
+		for (var attrname in mockFileLibrary.pathExists) { mergedFS[attrname] = mockFileLibrary.pathExists[attrname];}
 	}
 	if( fileWithContent )
 	{
@@ -241,8 +225,8 @@ function generateMockFsTestCases (pathExists,fileWithContent,funcName,args, recu
 	testCase += "\tsubject.{0}({1});\n".format(funcName, args );
 	testCase+="mock.restore();\n";
 	//testCase += "\tsubject.{0}({1});\n".format(funcName, args );
-	console.log(testCase);
 	var tempPathExists = mockFileLibrary.pathExists['path/fileExists'];
+
 	if(pathExists && recursion){
 		mockFileLibrary.pathExists['path/fileExists']={};
 		testCase += generateMockFsTestCases(pathExists,fileWithContent,funcName,args, false);
@@ -279,9 +263,7 @@ function constraints(filePath)
 			{
 				if( child.type === 'BinaryExpression' && child.operator == "=="){
 					if( child.left.type == 'Identifier' && child.right.type == "Literal")
-					{ console.log("Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
-						var expression = buf.substring(child.range[0], child.range[1]);
-						console.log("Buf:  child: {0} expression: {1}".format(JSON.stringify(child)),expression);
+					{ 	var expression = buf.substring(child.range[0], child.range[1]);
 						var rightHand = buf.substring(child.right.range[0], child.right.range[1])
 
 						functionConstraints[funcName].constraints.push(
@@ -302,12 +284,9 @@ function constraints(filePath)
 					if( child.left.type == 'Identifier' && params.indexOf( child.left.name ) > -1)
 					{
 						// get expression from original source code:
-						console.log("Child's range: "+JSON.stringify(child.range));
 						var expression = buf.substring(child.range[0], child.range[1]);
-						console.log("Buf:  child: {0} expression: {1}".format(JSON.stringify(child)),expression);
 						var rightHand = buf.substring(child.right.range[0], child.right.range[1])
-						console.log("Righthand :"+rightHand);
-						functionConstraints[funcName].constraints.push( 
+						functionConstraints[funcName].constraints.push(
 							new Constraint(
 							{
 								ident: child.left.name,
@@ -361,8 +340,7 @@ function constraints(filePath)
 				if( child.type == "CallExpression" && 
 					 child.callee.property &&
 					 child.callee.property.name =="readFileSync" )
-				{console.log("readFileSync!!!!!!!!!!!!!!!!!!!!");
-					for( var p =0; p < params.length; p++ )
+				{	for( var p =0; p < params.length; p++ )
 					{
 						if( child.arguments[0].name == params[p] )
 						{
@@ -405,8 +383,7 @@ function constraints(filePath)
 
 				if( child.type == "UnaryExpression" && child.argument.property &&
 					child.argument.property.name =="normalize")
-				{	console.log("unary nor");
-
+				{
 					functionConstraints[funcName].constraints.push(
 						new Constraint(
 							{
@@ -415,7 +392,7 @@ function constraints(filePath)
 								value:  "{\"normalize\" : true}",
 								funcName: funcName,
 								operator : child.operator,
-								expression: expression
+								kind: 'phoneNumber'
 							}))
 
 				}
@@ -423,8 +400,7 @@ function constraints(filePath)
 				if( child.type == "CallExpression" &&
 					//child.callee.property &&
 					child.callee.name =="normalize")
-				{	console.log("Normalize!!!!!!!!!!!!!!!!!!!");
-					for( var p =0; p < params.length; p++ )
+				{	for( var p =0; p < params.length; p++ )
 					{
 						if( child.arguments[0].name == params[p] )
 						{
@@ -436,7 +412,7 @@ function constraints(filePath)
 										value:  "\'"+faker.phone.phoneNumber()+"\'",
 										funcName: funcName,
 										operator : child.operator,
-										expression: expression
+										kind: 'phoneNumber'
 									}));
 						}
 					}
@@ -445,8 +421,7 @@ function constraints(filePath)
 				if( child.type == "CallExpression" &&
 						//child.callee.property &&
 					child.callee.name =="format")
-				{	console.log("Format!!!!!!!!!!!!!!!!!!!");
-					for( var p =0; p < params.length; p++ )
+				{	for( var p =0; p < params.length; p++ )
 					{
 						if( child.arguments[0].name == params[p] )
 						{
@@ -458,7 +433,7 @@ function constraints(filePath)
 										value:  "\'"+faker.phone.phoneNumber()+"\'",
 										funcName: funcName,
 										operator : child.operator,
-										expression: expression
+										kind: 'phoneNumber'
 									}));
 							//var areaNumber = ""+faker.phone.phoneNumber()+"";
 
@@ -529,4 +504,3 @@ if (!String.prototype.format) {
 }
 
 main();
-fakeDemo();
